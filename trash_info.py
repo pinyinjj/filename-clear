5c85dd3a1a13985da58c3d86609ba4c7b95bb22c
zzz <38111911+pinyinjj@users.noreply.github.com>
2024-12-08 23:08:13 +0800

    1

---
+++ trash_info.py
@@ -0,0 +1,16 @@
+patterns_to_remove = [
+    r'hhd800.com@',
+    r'nihao',
+    r'sehuatang@huluobuhe-',
+    r'',
+
+]
+
+files_to_remove = [
+    r'聚 合 全 網 H 直 播.html',
+    r'楼风最全资源.html',
+    r'最 新 位 址 獲 取.txt',
+    r'社 區 最 新 情 報.mp4',
+    r'x u u 6 2 . c o m.mp4'
+    r'test.txt'
+]
